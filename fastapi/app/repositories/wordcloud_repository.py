b1275dcbe2e0ca2e2956e0612a6685048b7c0658
june2598 <hydrlisk30@gmail.com>
2025-03-05 22:32:47 +0900

    20250305_wordcloudbugfix

--- fastapi/app/repositories/wordcloud_repository.py
+++ fastapi/app/repositories/wordcloud_repository.py
@@ -1,6 +1,3 @@
-# repositories 영역 : 데이터 베이스 관련 로직
-
-
 from sqlalchemy import create_engine
 import pandas as pd
 
@@ -18,9 +15,14 @@ def fetch_tokens_from_db():
     데이터베이스에서 뉴스 및 커뮤니티 토큰 데이터를 가져옴
     """
     query = """
-        SELECT TO_CHAR(NEWS_TOKEN) AS TOKEN FROM NEWS
+        SELECT NEWS_TOKEN AS TOKEN
+        FROM NEWS 
+        WHERE PUBLISHED_DATE >= SYSDATE - 2
         UNION ALL
-        SELECT TO_CHAR(CONTENT_TOKEN) AS TOKEN FROM COMMUNITY
+        SELECT CONTENT_TOKEN AS TOKEN
+        FROM COMMUNITY 
+        WHERE POST_DATE >= SYSDATE - 2
     """
-    df = pd.read_sql(query, con=engine)
+    with engine.connect() as connection:
+        df = pd.read_sql(query, con=connection)
     return df.dropna()    # 뉴스-토큰과 커뮤니티-토큰을 합친 데이터 프레임을 반환
